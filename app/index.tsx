7fc32aca294ee880a4a6080142dbcd965ef68980
Aymen Shoteri <95427313+AymenS02@users.noreply.github.com>
2026-03-17 11:12:49 -0400

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white text-2xl font-bold">
+        Edit app/indexsss.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
